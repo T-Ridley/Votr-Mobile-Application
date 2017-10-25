0681c4d34cbfb1f4bb8eb59239940a7b5d3aa007
T-Ridley <tr02816@georgiasouthern.edu>
2017-10-25 14:03:05 -0400

    First version of Votr Mobile Application. Home screen, welcome screen, and current poll screen designs implemented. Poll screen design in progress.

---
+++ app/src/main/java/tridley/android/votrmobileapplication/CurrentPollScreen.java
@@ -0,0 +1,13 @@
+package tridley.android.votrmobileapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentPollScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_poll_screen);
+    }
+}
---
+++ app/src/main/java/tridley/android/votrmobileapplication/PollScreen.java
@@ -0,0 +1,13 @@
+package tridley.android.votrmobileapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PollScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poll_screen);
+    }
+}
