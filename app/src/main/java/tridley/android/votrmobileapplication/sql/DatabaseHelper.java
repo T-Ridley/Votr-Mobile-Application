91a04efb8655af381f59d45f0ff51a5de4084455
Chessley <dh02988@georgiasouthern.edu>
2017-11-26 22:30:03 -0500

    New Database helper

--- app/src/main/java/tridley/android/votrmobileapplication/sql/DatabaseHelper.java
+++ app/src/main/java/tridley/android/votrmobileapplication/sql/DatabaseHelper.java
@@ -6,6 +6,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import tridley.android.votrmobileapplication.model.AccessCode;
+import tridley.android.votrmobileapplication.model.AccountInfo;
+import tridley.android.votrmobileapplication.model.Answers;
+import tridley.android.votrmobileapplication.model.CreatePoll;
+import tridley.android.votrmobileapplication.model.GivenAnswer;
+import tridley.android.votrmobileapplication.model.Poll;
+import tridley.android.votrmobileapplication.model.PollHistroy;
+import tridley.android.votrmobileapplication.model.Questions;
 import tridley.android.votrmobileapplication.model.User;
 
 /**
@@ -15,20 +23,117 @@ import tridley.android.votrmobileapplication.model.User;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
-    private static final String DATABASE_NAME = "UserManager.db"; //Create Database
+    private static final String DATABASE_NAME = "UserManager.db";
 
-    private static final String TABLE_USER = "user"; // Create User entity
+    private static final String TABLE_USER = "user";
+    private static final String TABLE_ACCESSCODE = "access code";
+    private static final String TABLE_ACCOUNTINFO = "account info";
+    private static final String TABLE_ANSWERS= "answers";
+    private static final String TABLE_CREATEPOLL = "create poll";
+    private static final String TABLE_GIVENANSWER = "given answer";
+    private static final String TABLE_POLL = "poll";
+    private static final String TABLE_POLLHISTORY = "poll history";
+    private static final String TABLE_QUESTIONS= "questions";
 
-    private static final String COLUMN_USER_ID = "user_id"; // User entity attributes
+//access code table
+    private static final String COLUMN_ACCESS_CODE = "access_code";
+
+    private String CREATE_ACCESSCODE_TABLE = "CREATE TABLE " + TABLE_ACCESSCODE + "("
+            + COLUMN_ACCESS_CODE + " INTEGER PRIMARY KEY AUTOINCREMENT," + ")";
+
+    private String DROP_ACCESSCODE_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+    //account info table
+    private static final String COLUMN_ACCOUNT_ID = "account id";
+
+    private String CREATE_ACCOUNTINFO_TABLE = "CREATE TABLE " + TABLE_ACCOUNTINFO + "("
+            + COLUMN_ACCOUNT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + ")";
+
+    private String DROP_ACCOUNTINFO_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+    //answers table
+    private static final String COLUMN_ANSWER = "answer";
+
+    private String CREATE_ANSWERS_TABLE = "CREATE TABLE " + TABLE_ANSWERS + "("
+        + COLUMN_ANSWER + " TEXT," + ")";
+
+    private String DROP_ANSWERS_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+//poll table
+
+    private static final String COLUMN_POLLTITLE = "title";
+    private static final String COLUMN_POLLANSWER_ = "answer";
+    private static final String COLUMN_POLLQUESTION = "question";
+
+
+    private String CREATE_POLL_TABLE = "CREATE TABLE " + TABLE_POLL + "("
+            + COLUMN_POLLTITLE + " TEXT," + COLUMN_POLLANSWER_ + " TEXT,"
+            + COLUMN_POLLQUESTION + "TEXT," + ")";
+
+    private String DROP_POLL_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+//given answer table
+
+    private static final String COLUMN_GIVENANSWER = "given answers";
+
+    private String CREATE_GIVENANSWER_TABLE = "CREATE TABLE " + TABLE_GIVENANSWER + "("
+            + COLUMN_GIVENANSWER + " TEXT," + ")";
+
+    private String DROP_GIVENANSWER_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+    //create poll table
+    private static final String COLUMN_CREATEPOLL_ID = "poll id";
+    private static final String COLUMN_CREATEPOLL_TITLE = "title";
+    private static final String COLUMN_CREATEPOLL_CODE = "code";
+    private static final String COLUMN_CREATEPOLL_OPTIONS = "options";
+
+
+    private String CREATE_CREATEPOLL_TABLE = "CREATE TABLE " + TABLE_CREATEPOLL + "("
+            + COLUMN_CREATEPOLL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_CREATEPOLL_TITLE+ " TEXT,"
+            + COLUMN_CREATEPOLL_CODE + "INTEGER," + COLUMN_CREATEPOLL_OPTIONS + "TEXT" + ")";
+
+    private String DROP_CREATEPOLL_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+    //poll history table
+    private static final String COLUMN_POLLHISTORY_TITLE= "title";
+
+    private String CREATE_POLLHISTORY_TABLE = "CREATE TABLE " + TABLE_POLLHISTORY + "("
+            + COLUMN_POLLHISTORY_TITLE + " TEXT," + ")";
+
+    private String DROP_POLLHISTORY_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+    //questions table
+    private static final String COLUMN_QUESTIONS = "categories";
+
+    private String CREATE_QUESTIONS_TABLE = "CREATE TABLE " + TABLE_QUESTIONS + "("
+            + COLUMN_QUESTIONS + " TEXT," + ")";
+
+    private String DROP_QUESTIONS_TABLE = "DROP TABLE IF EXISTS " + TABLE_QUESTIONS;
+
+
+
+
+
+
+
+    //user table
+    private static final String COLUMN_USER_ID = "user_id";
     private static final String COLUMN_USER_NAME = "user_name";
     private static final String COLUMN_USER_EMAIL = "user_email";
     private static final String COLUMN_USER_PASSWORD = "user_password";
 
-    private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + " ("
-            +COLUMN_USER_ID+ " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
-            + COLUMN_USER_EMAIL+ " TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ");"; //Putting values into entity
+    private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
+            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
+            + COLUMN_USER_EMAIL + "TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ")";
 
-    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER; //Dropping user table set if exists
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER;
 
     public DatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -37,13 +142,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db){
         db.execSQL(CREATE_USER_TABLE);
+        db.execSQL(CREATE_ACCESSCODE_TABLE);
+        db.execSQL(CREATE_ACCOUNTINFO_TABLE);
+        db.execSQL(CREATE_ANSWERS_TABLE);
+        db.execSQL(CREATE_CREATEPOLL_TABLE);
+        db.execSQL(CREATE_GIVENANSWER_TABLE);
+        db.execSQL(CREATE_POLLHISTORY_TABLE);
+        db.execSQL(CREATE_POLL_TABLE);
+        db.execSQL(CREATE_QUESTIONS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        // Dropping older table if exists
         db.execSQL(DROP_USER_TABLE);
-        // Create tables again
+        db.execSQL(DROP_ACCESSCODE_TABLE);
+        db.execSQL(DROP_ACCOUNTINFO_TABLE);
+        db.execSQL(DROP_ANSWERS_TABLE);
+        db.execSQL(DROP_CREATEPOLL_TABLE);
+        db.execSQL(DROP_GIVENANSWER_TABLE);
+        db.execSQL(DROP_POLLHISTORY_TABLE);
+        db.execSQL(DROP_POLL_TABLE);
+        db.execSQL(DROP_QUESTIONS_TABLE);
         onCreate(db);
     }
 
@@ -107,6 +226,97 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
         return false;
     }
+//accesscode populate
+public void addAccessCode(AccessCode code){
+    SQLiteDatabase db = this.getWritableDatabase();
+    ContentValues values = new ContentValues();
 
+    values.put(COLUMN_ACCESS_CODE, code.getAccessCode());
 
+    db.insert(TABLE_ACCESSCODE, null, values);
+    db.close();
+
+}
+//accountinfo populate
+public void addAccountInfo(AccountInfo account){
+    SQLiteDatabase db = this.getWritableDatabase();
+    ContentValues values = new ContentValues();
+
+    values.put(COLUMN_ACCOUNT_ID, account.getId());
+
+    db.insert(TABLE_ACCOUNTINFO, null, values);
+    db.close();
+
+}
+    //answers populate
+    public void addAnwers(Answers answers){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_ANSWER, answers.getAnswer());
+
+        db.insert(TABLE_ANSWERS, null, values);
+        db.close();
+
+    }
+    //createpoll populate
+    public void addCreatePoll(CreatePoll createPoll){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_CREATEPOLL_CODE, createPoll.getCode());
+        values.put(COLUMN_CREATEPOLL_ID, createPoll.getId());
+        values.put(COLUMN_CREATEPOLL_OPTIONS, createPoll.getOptions());
+        values.put(COLUMN_CREATEPOLL_TITLE, createPoll.getTitle());
+
+        db.insert(TABLE_CREATEPOLL, null, values);
+        db.close();
+
+    }
+    //givenanswer populate
+    public void addGivenAnswer(GivenAnswer givenAnswer){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_GIVENANSWER, givenAnswer.getGivenAnswer());
+
+        db.insert(TABLE_GIVENANSWER, null, values);
+        db.close();
+
+    }
+    //poll populate
+    public void addPoll(Poll poll){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_POLLTITLE, poll.getPollTitle());
+        values.put(COLUMN_POLLANSWER_, poll.getPollAnswer());
+        values.put(COLUMN_POLLQUESTION, poll.getPollQuestion());
+
+        db.insert(TABLE_POLL, null, values);
+        db.close();
+
+    }
+    //pollhitory populate
+    public void addPollHistory(PollHistroy pollHistroy){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_POLLHISTORY_TITLE, pollHistroy.getTitle());
+
+        db.insert(TABLE_POLLHISTORY, null, values);
+        db.close();
+
+    }
+    //questions populate
+    public void addQuestions(Questions questions){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_QUESTIONS, questions.getQuestion());
+
+        db.insert(TABLE_QUESTIONS, null, values);
+        db.close();
+
+    }
 }
